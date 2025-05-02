58893e85ddb30abc5a7fa657f3eb807426ab90b4
krish-able <krishna@able.do>
2025-05-03 02:46:12 +0530

    chore: merges main

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import About from '$lib/components/About.svelte';
 	import Hero from '$lib/components/Hero.svelte';
-import Hero from '$lib/components/Hero.svelte';
 </script>
 
 <Hero />
