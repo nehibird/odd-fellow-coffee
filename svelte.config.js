432d5d954f0cbe600b31f967ce078b88e73d8c8b
krish-able <krishna@able.do>
2025-05-10 23:39:15 +0530

    chore: adds prerender-true

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,8 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+let entries = [];
+entries = ['/'];
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,7 +14,13 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			strict: false
+		}),
+		prerender: {
+			entries: entries,
+			handleHttpError: 'warn'
+		}
 	}
 };
 
