556d1883f24d1e2adca3cc7c1bdbf2ff98e1cbfb
nehibird <nehi@birdherd.media>
2025-05-14 00:09:48 +0530

    Delete src directory

--- src/routes/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import About from '$lib/components/About.svelte';
-	import Gallery from '$lib/components/Gallery.svelte';
-	import Hero from '$lib/components/Hero.svelte';
-</script>
-
-<Hero />
-<About />
-<Gallery />
