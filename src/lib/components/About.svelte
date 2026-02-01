51b27764696d6ce2b134447eab96a7943a7b150d
nehibird <nehi@birdherd.media>
2026-01-31 19:32:07 -0600

    Initial SvelteKit scaffold from review repo

---
+++ src/lib/components/About.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+</script>
+
+<section class="mx-auto max-w-screen-xl px-8 py-6 md:py-10">
+	<h3 class="w-full pb-6 text-left text-4xl capitalize md:pb-10">
+		<span class="text-medium-carmine">a</span>bout
+	</h3>
+
+	<div class="grid grid-cols-1 gap-y-10 md:grid-cols-3">
+		<div class="order-1 hidden md:col-span-2 md:block">
+			<img
+				src="/assets/images/about/about-2.jpg"
+				alt="about-banner"
+				class="h-full w-full rounded-br-full rounded-tl-[100rem]"
+			/>
+		</div>
+		<div class="relative order-2 col-span-1">
+			<img
+				src="/assets/images/about/about-1.jpg"
+				alt="about"
+				class="rounded-l-0 h-full w-full rounded-l-full rounded-r-[100rem] object-cover"
+			/>
+			<img
+				src="/assets/logo-rounded.svg"
+				alt=""
+				class="absolute left-0 top-0 z-10 w-20 rounded-full shadow-2xl shadow-black md:-left-12 md:top-1/2 md:-translate-y-1/2"
+			/>
+		</div>
+		<p class="order-3 col-span-1 text-base md:col-span-3">
+			Welcome to Odd Fellow Coffee Roasters, where every Monday means freshly roasted coffee beans
+			and hand-crafted sourdough bread. Caleb roasts the coffee, Deborah bakes the bread, and
+			together we’re committed to bringing you simple, quality goods made with care. Orders open
+			each Monday—just choose your favorite and we’ll take care of the rest. We’re proud to be part
+			of Oklahoma’s growing community of makers, growers, and neighbors who value quality over
+			convenience. Odd Fellow isn’t just about what we make—it’s about who we make it for. Whether
+			you’re down the road or across the state, we’re honored to share a little bit of our home with
+			yours
+		</p>
+	</div>
+</section>
